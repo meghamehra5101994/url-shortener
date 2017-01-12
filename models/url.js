76e47e3df74f7521109dbe2f025e74ac999c61c9
Nitin Kumar Singh <nitinksingh11@gmail.com>
2017-01-12 12:24:40 +0530

    updating model for counter

--- models/url.js
+++ models/url.js
@@ -1,9 +1,34 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+// schema for counter
+var counterSchema = Schema({
+    _id: {type: String, required: true},
+    seq: { type: Number, default: 0 }
+});
+
+
+
 // create a schema for urls
-var urlSchema = new Schema({
+var miniurlSchema = new Schema({
   _id: {type: Number, index: true},
   long_url: String,
   created_at: Date
 });
+
+
+// before inserting function
+miniurlSchema.pre('save', function(next){
+  var self = this;
+  counter.findByIdAndUpdate({_id: 'url_count'}, {$inc: {seq: 1} }, function(error, counter) {
+      if (error)
+          return next(error);
+      self.created_at = new Date();
+      self._id = counter.seq;
+      next();
+  });
+});
+var counter = mongoose.model('counter', counterSchema);
+var Url = mongoose.model('Url', miniurlSchema);
+
+module.exports = Url;
