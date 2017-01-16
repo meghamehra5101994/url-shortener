79cdeadc0e791670e07e59a80a35fae25a59d4d3
Nitin Kumar Singh <nitinksingh11@gmail.com>
2017-01-16 16:08:40 +0530

    updating confug with app url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 var config = {};
-config.webhost = 'http://localhost:3000/';
+config.webhost = process.env.APPURL || 'http://localhost:3000/';
 config.dbpath = process.env.DBPATH || 'mongodb://localhost:27017/test';
 module.exports = config;
